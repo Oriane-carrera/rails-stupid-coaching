0a621e137a3acabe277d6d90b939fe2d2c865530
Oriane Carrera <oriane.carrera@gmail.com>
2025-11-24 15:41:07 +0100

    stupid coach

---
+++ test/controllers/question_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class QuestionControllerTest < ActionDispatch::IntegrationTest
+  test "should get ask" do
+    get question_ask_url
+    assert_response :success
+  end
+
+  test "should get answer" do
+    get question_answer_url
+    assert_response :success
+  end
+end
